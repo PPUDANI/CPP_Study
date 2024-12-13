28ea94c7f78850b59469d5d4a89d79d7f7a496ae
PPUDANI <zxvk1030@naver.com>
2024-12-13 18:48:05 +0900

    업데이트

--- TestProject/TestProject.cpp
+++ TestProject/TestProject.cpp
@@ -1,24 +1,19 @@
 ﻿#include <iostream>
-#include <vector>
-#include <string>
-using namespace std;
-
-int add(int a, int b)
-{
-	cout << &a << &b << endl;
-	int result = a + b;
-	return result;
-}
-
+#include <chrono>
 
 int main()
 {
-	/*for (int i = 0; i < 10000; ++i)
-	{
-		add(i, i + 5);
-	}*/
+    //포맷을 사용해서 출력 가능
+    std::chrono::zoned_time zt{ std::chrono::current_zone(), std::chrono::system_clock::now() };
 
-	vector<int> vec(10, 0);
-	int a = vec[2];
-	int b = vec[3];
+    std::cout << std::format("{:%yy.%m.%d %A %H:%M:%S}", zt) << std::endl;
+
+    //연도 월 날짜 등을 지원하는 클래스
+    const std::chrono::time_point now{ std::chrono::system_clock::now() };
+    const std::chrono::year_month_day ymd{ std::chrono::floor<std::chrono::days>(now) };
+
+    std::cout << "Current Year: " << static_cast<int>(ymd.year()) << " Month: " << static_cast<unsigned>(ymd.month()) << " Day: " << static_cast<unsigned>(ymd.day()) << "\n" << "ymd: " << ymd << '\n';
+
+
+    return 0;
 }
