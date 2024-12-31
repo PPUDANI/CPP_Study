98cb26c6be8b2cdad2b44cc17d73b0eb6b859e7b
PPUDANI <zxvk1030@naver.com>
2024-12-31 16:43:18 +0900

    업데이트

--- Little_And_Big_Endian/Little_And_Big_Endian.cpp
+++ Little_And_Big_Endian/Little_And_Big_Endian.cpp
@@ -6,8 +6,8 @@ int main()
     int arr[2] = { 0, };
     //0b 00000001 00000010 00000011 00000100
     arr[0] = 0b00000001000000100000001100000100;
-    char* ptr = reinterpret_cast<char*>(&arr[0]);
-
+    __int8* ptr = reinterpret_cast<__int8*>(&arr[0]);
+    
     cout << static_cast<int>(*(ptr)) << endl;
     ptr++;
     cout << static_cast<int>(*(ptr)) << endl;
